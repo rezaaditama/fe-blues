226d4bd70e4cdc29cfd392014c382ec8486046dc
Reza Aditama <aditamareza03@gmail.com>
2025-06-07 22:13:14 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className='w-full bg-blue-700 min-h-screen flex justify-center items-center'>
+      <p className='text-white'>Hello World</p>
+    </div>
+  );
+};
+
+export default App;
